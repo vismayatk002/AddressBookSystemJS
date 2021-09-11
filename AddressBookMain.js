b84eeb6b8947df85c0ba01accef75ae24fed206f
vismayatk002 <vismayatk002@gmail.com>
2021-09-11 21:18:09 +0530

    Refactor avoided duplication entries

--- AddressBookMain.js
+++ AddressBookMain.js
@@ -13,40 +13,64 @@ function addContact(){
     
         let firstName = prompt('Enter First Name : ');
         contact.validateName(firstName);
-        contact.firstName = firstName;
-    
-        let lastName = prompt('Enter Last Name : ');
-        contact.validateName(lastName);
-        contact.lastName = lastName;
-    
-        let address = prompt('Enter Address : ');
-        contact.validateAddress(address);
-        contact.address = address;
-    
-        // let city = prompt('Enter City : ');
-        // contact.validateAddress(city);
-        // contact.city = city;
-    
-        // let state = prompt('Enter State : ');
-        // contact.validateAddress(state);
-        // contact.state = state;
-    
-        // let zip = prompt('Enter Zip : ');
-        // contact.validateZip(zip)
-        // contact.zip = zip;
-    
-        // let phoneNo = prompt('Enter Phone Nmuber : ');
-        // contact.validatePhoneNo(phoneNo);
-        // contact.phoneNo = phoneNo;
-    
-        // let email = prompt('Enter E-Mail : ');
-        // contact.validateEmail(email)
-        // contact.email = email;
-    
-        addInAddrBook(contact,addrName);
+        if(isPersonExist(firstName)){
+
+            contact.firstName = firstName;
+        
+            let lastName = prompt('Enter Last Name : ');
+            contact.validateName(lastName);
+            contact.lastName = lastName;
+        
+            let address = prompt('Enter Address : ');
+            contact.validateAddress(address);
+            contact.address = address;
+        
+            let city = prompt('Enter City : ');
+            contact.validateAddress(city);
+            contact.city = city;
+        
+            let state = prompt('Enter State : ');
+            contact.validateAddress(state);
+            contact.state = state;
+        
+            let zip = prompt('Enter Zip : ');
+            contact.validateZip(zip)
+            contact.zip = zip;
+        
+            let phoneNo = prompt('Enter Phone Nmuber : ');
+            contact.validatePhoneNo(phoneNo);
+            contact.phoneNo = phoneNo;
+        
+            let email = prompt('Enter E-Mail : ');
+            contact.validateEmail(email)
+            contact.email = email;
+        
+            addInAddrBook(contact,addrName);
+            console.log("Your Contact added ..");
+        }
+        else{
+            console.log("Contact already exist !");
+        }
         
     }catch(err) {
         console.log(err);
+    }
+}
+
+function isPersonExist(firstName){
+
+    let flag = 1;
+    for(const book of addrArr){
+        
+        if(book.contactArr.some(contact => contact.firstName === firstName)){
+            flag = 0;
+        }
+    }
+    if(flag == 1){
+        return true;
+    }
+    else{
+        return false;
     }
 }
 
